6cefa2eaff47bba0543fbeb44f9163394cdaf3c7
Somi Kim <303vivienne@naver.com>
2024-05-30 15:00:32 +0200

    first commit

--- UknowCoding/05_timer/script.js
+++ UknowCoding/05_timer/script.js
@@ -1,3 +1,33 @@
+// 다시 연습
+const time = document.getElementById('time');
+const stopBtn = document.querySelector('.stop');
+const goBtn = document.querySelector('.go');
+let timeOn;
+
+function getTime() { 
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    
+    time.textContent = `${hours}:${minutes}:${seconds}`    
+}
+getTime();
+timeOn = setInterval(getTime, 1000);
+goBtn.onclick = function () { 
+    setInterval(getTime, 1000);
+}
+
+stopBtn.onclick = function () { 
+    clearInterval(timeOn);
+}
+
+
+/* 첫번째d
 let handleId = 0; // 시계가 움직일 경우 동작에 대한 ID
 const $h1 = document.getElementById("time");
 const stopBtn = document.querySelector('.stop');
@@ -24,3 +54,4 @@ stopBtn.onclick = function () {
     clearInterval(handleId);
     handleId = 0;    
 }
+*/
