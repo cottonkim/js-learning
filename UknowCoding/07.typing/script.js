d9ed86cf9b16dc1f363f867bbeb665f7ce369f84
Somi Kim <303vivienne@naver.com>
2024-06-04 22:09:19 +0200

    first commit

--- UknowCoding/07.typing/script.js
+++ UknowCoding/07.typing/script.js
@@ -1,3 +1,18 @@
+const content = `I'm Somi, Frond-End Developer`;
+const text = document.getElementById('text')
+let index = 0; // content 문자열에 접근하기 위한 인덱스
+
+function typing() { 
+    text.textContent += content[index++];
+    if (index > content.length) { 
+        text.textContent = '';
+        index = 0;
+    }
+}
+
+setInterval(typing, 300)
+
+/*
 const content = 'Typing Effect';
 let text = document.getElementById('text');
 let index = 0;
@@ -11,3 +26,4 @@ function typing() {
 }
 
 setInterval(typing, 500);
+*/
